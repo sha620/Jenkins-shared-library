f79f4e1513794839405fd6fe9cdae850bed2bdca
sha620 <shaktivicky91@gmail.com>
2025-08-16 09:26:47 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -6,8 +6,8 @@ def call(String credid, String imagename ){
                passwordVariable: "pass"
                )]){
                    sh "docker login -u ${env.user} -p ${env.pass}"
-                   sh "docker image tag ${imagename} ${env.user}/${imagename} "
-                   sh "docker push ${env.user}/${imagename}"
+                   sh "docker image tag "${imagename}" ${env.user}/${imagename}"
+                   sh "docker push ${env.user}/${imagename}:latest"
                }
             }
 }
