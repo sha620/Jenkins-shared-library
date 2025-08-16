0402e14052ed5cbef1bfee1a2340e9cf0eb3526f
sha620 <shaktivicky91@gmail.com>
2025-08-16 09:14:02 +0530

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,13 @@
+def call(String credid, String imagename ){
+  steps{
+               withCredentials([usernamePassword(
+               credentialsId: "${credid}",
+               usernameVariable: "user",
+               passwordVariable: "pass"
+               )]){
+                   sh "docker login -u ${env.user} -p ${env.pass}"
+                   sh "docker image tag ${imagename} ${env.user}/${imagename} "
+                   sh "docker push ${env.user}/${imagename}"
+               }
+            }
+}
